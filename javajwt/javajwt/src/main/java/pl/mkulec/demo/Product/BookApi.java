08aedc7ec70ee557bb4ded25f7a18f32709288d8
Mateusz Kulec <101333426+MateuszRL96@users.noreply.github.com>
2023-12-12 20:34:17 +0100

    df

--- javajwt/javajwt/src/main/java/pl/mkulec/demo/Product/BookApi.java
+++ javajwt/javajwt/src/main/java/pl/mkulec/demo/Product/BookApi.java
@@ -1,17 +1,19 @@
 package pl.mkulec.demo.Product;
 
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
+//@CrossOrigin(origins = "localhost:4200")
 @RestController
 public class BookApi {
 
 
-    @GetMapping()
+    @GetMapping("/api/books")
     public List<Book> get()
     {
 
